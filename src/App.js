3dc762d5f4a9820fb4ca123b5d159942133a9b76
Nithin <106112831+tamnurunithin@users.noreply.github.com>
2025-02-07 13:02:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="counter-container">
+      <h1 style={{ color: "black" }}>Counter</h1>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+
+export default App;
